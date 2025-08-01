3aba5bd29b4eee87dbddff7415a4c3a98ae4becb
annaguerrra <annaguerracastro@gmail.com>
2025-08-01 14:49:56 -0300

    use case post

--- UseCases/CreatePost/CreatePostUseCase.cs
+++ UseCases/CreatePost/CreatePostUseCase.cs
@@ -1,8 +1,13 @@
+using Fofoquinha_API.UseCases.CreatePost;
 using FofoquinhaAPI.Models;
+using Microsoft.VisualBasic;
 
 namespace Fofoquinha_API.UseCases.CreateProfile;
 
-public class CreatePost
+public class CreatePostUseCase(FofoquinhaAPIDbContext context)
 {
-    
+    public async Task<Result<CreatePostResponse>> Do(CreatePostPayload payload)
+    {
+        return Result<CreatePostResponse>.Ok(null);
+    }    
 }
